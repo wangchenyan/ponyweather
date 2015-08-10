8e7c0286498642f241c7829aa21815758546edd2
unknown <chenyan.wang@QF-wangchenyan.qunarservers.com>
2015-08-10 18:44:16 +0800

    1.readd butterknife & config proguard 2.use external library

--- app/src/main/java/me/wcy/weather/activity/BaseActivity.java
+++ app/src/main/java/me/wcy/weather/activity/BaseActivity.java
@@ -3,7 +3,7 @@ package me.wcy.weather.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import me.wcy.weather.util.ViewInjector;
+import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
@@ -16,7 +16,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     public void setContentView(View view) {
         super.setContentView(view);
-        ViewInjector.initInjectedView(this);
+        ButterKnife.bind(this);
     }
 
 }
