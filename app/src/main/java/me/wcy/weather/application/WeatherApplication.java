0a414c5676107d90f3ea7d475b04aea64da50d2f
hzwangchenyan <hzwangchenyan@corp.netease.com>
2016-04-12 20:20:39 +0800

    自动夜间模式

--- app/src/main/java/me/wcy/weather/application/WeatherApplication.java
+++ app/src/main/java/me/wcy/weather/application/WeatherApplication.java
@@ -1,9 +1,14 @@
 package me.wcy.weather.application;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import java.util.Calendar;
 
 import cn.bmob.v3.Bmob;
 import im.fir.sdk.FIR;
@@ -19,6 +24,7 @@ public class WeatherApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        initNightMode();
         ScreenUtils.init(this);
         Bmob.initialize(this, ApiKey.BMOB_KEY);
         FIR.init(this);
@@ -26,5 +32,19 @@ public class WeatherApplication extends Application {
                 .memoryCacheSize(2 * 1024 * 1024)
                 .diskCacheSize(50 * 1024 * 1024)
                 .build());
+    }
+
+    private void initNightMode() {
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        updateNightMode(hour >= 19 || hour < 7);
+    }
+
+    private void updateNightMode(boolean on) {
+        Resources sRes = getResources();
+        DisplayMetrics dm = sRes.getDisplayMetrics();
+        Configuration config = sRes.getConfiguration();
+        config.uiMode &= ~Configuration.UI_MODE_NIGHT_MASK;
+        config.uiMode |= on ? Configuration.UI_MODE_NIGHT_YES : Configuration.UI_MODE_NIGHT_NO;
+        sRes.updateConfiguration(config, dm);
     }
 }
